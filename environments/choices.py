5d8fda938e15790a1d6908b549c191f6288cd04a
acohon <showwinho@gmail.com>
2024-09-20 19:44:57 +0800

    add booking date & time in html

--- environments/choices.py
+++ environments/choices.py
@@ -1,5 +1,7 @@
-title_choices = {
-    'visiting': 預約參觀院舍,
-    'booking': 預約到訪院舍選位,
+from datetime import datetime, timedelta
 
+# 定義標題選項
+title_choices = {
+    'visiting': '預約參觀選位',
 }
+
--- environments/models.py
+++ environments/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from datetime import datetime
+from environments.choices import title_choices
 
 
 # Create your models here.
 class Booking(models.Model):
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=20,choices=title_choices.items())
     name = models.CharField(max_length=50)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
-    visit_date_time = models.DateTimeField
+    visit_date_time = models.DateTimeField(default=datetime.now, blank=True)
     comment = models.TextField(blank=True)
     submit_date = models.DateTimeField(default=datetime.now, blank=True)
     
