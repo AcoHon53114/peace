a6c39ef3a4cbce940a6feafd986a79fd26fa9b93
acohon <showwinho@gmail.com>
2024-09-27 06:28:25 +0800

    updated news.html, new.html, url.py of news and views.py of news from Cindy

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.index, name='news'),
-    path('<int:new_id>',views.new, name ='new'),
-    #path('search',views.search, name='search')
-
+    path('<int:new_id>', views.new, name='new'),
+    path('search', views.search, name='search'), 
 ]
--- news/views.py
+++ news/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .models import New
+from django.shortcuts import render, get_object_or_404
+from urllib.parse import urlparse, parse_qs
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'news/news.html')
+    news = New.objects.order_by('-list_date').filter(is_published=True)
+    #get all data from listing database
+    paginator = Paginator(news, 3)
+    page = request.GET.get('page')
+    print(page)
+    paged_news = paginator.get_page(page)
+    
+    context = {'news' : paged_news}
+    return render(request, 'news/news.html', context)
 
-def new(request):
-    return render(request, 'news/new.html')
+def new(request, new_id):
+    new = get_object_or_404(New, pk=new_id)
+    context = {'new': new}
+    print(f"YouTube URL: {new.youtube_link}")
+    return render(request,'news/new.html', context)
+
+def search(request):
+    queryset_list = New.objects.order_by('-list_date')
+    if 'title' in request.GET:
+        title = request.GET['title']
+        if title:
+            queryset_list = queryset_list.filter(
+                title__icontains=title
+                )
+        context = {
+                'news':queryset_list,
+                'values':request.GET,
+                }
+    return render(request,'news/news.html', context)
