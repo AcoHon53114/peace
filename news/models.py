ed24ab65fa9a27e05317c3e8df8eb871c91ec912
acohon <showwinho@gmail.com>
2024-09-27 04:37:36 +0800

    updated templatemo-kind-heart-charity.css and models.py field of news

--- news/models.py
+++ news/models.py
@@ -3,10 +3,10 @@ from datetime import datetime
 
 # Create your models here.
 class New(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=14)
     description = models.TextField(blank=True)
     is_published = models.BooleanField(default=True)
-    youtube_link = models.URLField(max_length=300)
+    youtube_link = models.URLField(max_length=1000, blank=True)
     list_date = models.DateTimeField(auto_now_add=True)
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
     photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
